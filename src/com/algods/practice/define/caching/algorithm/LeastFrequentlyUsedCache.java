6795fb999e3e548e8d58e489e49a9415c84cbd78
ankitasinha0530-root <45222337+ankitasinha0530-root@users.noreply.github.com>
2026-05-12 05:01:25 +0530

    Initial commit

---
+++ src/com/algods/practice/define/caching/algorithm/LeastFrequentlyUsedCache.java
@@ -0,0 +1,5 @@
+package com.algods.practice.define.caching.algorithm;
+
+public class LeastFrequentlyUsedCache {
+
+}
---
+++ src/com/algods/practice/graph/ds/FKruskalsAlgorithm.java
@@ -0,0 +1,10 @@
+package com.algods.practice.graph.ds;
+
+public class FKruskalsAlgorithm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
